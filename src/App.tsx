7797c8af1adb9b3fbf04ffa0e87d8df6d6ddac02
kimkyuhong <bobo3766@naver.com>
2025-06-12 18:49:35 +0900

    init: tanstack-query 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import '@/App.css';
+import queryClient from '@/apis/queryClient';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <div>DASH</div>
+      </QueryClientProvider>
+    </>
+  );
 }
 
 export default App;
---
+++ src/apis/queryClient.ts
@@ -0,0 +1,10 @@
+import { QueryClient } from '@tanstack/react-query';
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+export default queryClient;
