2906bed45321843c7951b69073725891010c9eec
namdaeun <nde40345@gmail.com>
2025-06-13 00:00:38 +0900

    chore: 불필요한 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import '@/App.css';
 import queryClient from '@/apis/queryClient';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
   return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} />
-        <div>DASH</div>
-      </QueryClientProvider>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
