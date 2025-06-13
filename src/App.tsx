da2719ea1c589b9ec51c7aae945591e748126688
kimkyuhong <bobo3766@naver.com>
2025-06-13 18:18:27 +0900

    style: mds font, color tailwind 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import queryClient from '@/apis/queryClient';
+import Header from '@/components/Header';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import '@/App.css';
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
+      <Header />
     </QueryClientProvider>
   );
 }
