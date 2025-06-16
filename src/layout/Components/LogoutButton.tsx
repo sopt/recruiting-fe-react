a1a0d8a08d64a0247a904d47046a0ec1276c1e5f
kimkyuhong <bobo3766@naver.com>
2025-06-16 16:56:18 +0900

    style: 버튼 배경색 변경

---
+++ src/layout/Components/LogoutButton.tsx
@@ -0,0 +1,12 @@
+const LogoutButton = () => {
+  return (
+    <button
+      type="button"
+      className="px-[2.4rem] py-[0.7rem] rounded-[19px] bg-gray800 text-gray200 text-[1.6rem] font-semibold leading-[2.4rem] tracking-[-0.24px]"
+    >
+      로그아웃
+    </button>
+  );
+};
+
+export default LogoutButton;
