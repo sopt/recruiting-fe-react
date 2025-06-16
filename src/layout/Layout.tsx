4b339d0c51511a1fd94c5094f77470eb4e657aa7
kimkyuhong <bobo3766@naver.com>
2025-06-16 17:29:28 +0900

    feat: 레이아웃 컴포넌트 생성

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,10 +3,10 @@ import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
-    <>
+    <main className="ml-[12.4rem]">
       <CommonHeader />
       <Outlet />
-    </>
+    </main>
   );
 };
 
