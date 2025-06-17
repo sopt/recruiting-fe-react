6124a9a91963be541f14ba053166b38848675169
kimkyuhong <bobo3766@naver.com>
2025-06-18 00:59:27 +0900

    feat: 설명글 등록 컴포넌트 구현

--- src/pages/PostQuestion/PostQuestion.tsx
+++ src/pages/PostQuestion/PostQuestion.tsx
@@ -1,8 +1,43 @@
+import { Add } from '@/assets/svg';
+import DescriptionBox from '@/pages/PostQuestion/components/DescriptionBox';
 import QuestionBox from '@/pages/PostQuestion/components/QuestionBox';
+import { Button } from '@sopt-makers/ui';
+import { useState } from 'react';
 
 const PostQuestion = () => {
+  const [hasDescription, setHasDescription] = useState(false);
+
+  const handleHasDescriptionChange = (bool: boolean) => {
+    setHasDescription(bool);
+  };
+
   return (
-    <main>
+    <main className="max-w-[98rem]">
+      <div className="flex justify-between items-end mb-[2rem]">
+        <span className="title_6_16_sb text-gray200">총 2개</span>
+        <div className="flex gap-[1.6rem]">
+          <Button variant="outlined" size="md">
+            임시저장
+          </Button>
+          <Button variant="fill" size="md">
+            최종 등록하기
+          </Button>
+        </div>
+      </div>
+
+      {hasDescription ? (
+        <DescriptionBox onHasDescriptionChange={handleHasDescriptionChange} />
+      ) : (
+        <button
+          type="button"
+          onClick={() => handleHasDescriptionChange(true)}
+          className="flex flex-row gap-[0.4rem] mb-[3.2rem] px-[2rem] py-[1.2rem] rounded-[10px] bg-gray700 label_2_16_sb cursor-pointer"
+        >
+          <Add width={20} />
+          설명글 추가하기
+        </button>
+      )}
+
       <QuestionBox />
     </main>
   );
