07dff645840cba04c7dd5e4fe3564368c764e326
kimkyuhong <bobo3766@naver.com>
2025-06-17 21:09:08 +0900

    chore: svg 추가

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,2 +1,6 @@
 export { default as Arrange } from './Arrange';
+export { default as Add } from './Add';
+export { default as Check } from './Check';
 export { default as InfoCircle } from './InfoCircle';
+export { default as Link } from './Link';
+export { default as Trash } from './Trash';
