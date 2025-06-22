1a5c77dd44e84a42fe74e9ca703fd360f5f091ba
namdaeun <nde40345@gmail.com>
2025-06-22 17:58:50 +0900

    feat: tailwind scrollbar hide 설치

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import scrollbarHide from 'tailwind-scrollbar-hide';
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  plugins: [scrollbarHide],
+} satisfies Config;
