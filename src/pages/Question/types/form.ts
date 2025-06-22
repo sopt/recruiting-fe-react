0e6233e0c7983e1a4f50beb80bdd0c440a6be386
kimkyuhong <bobo3766@naver.com>
2025-06-22 18:13:13 +0900

    feat: 질문 schema 생성

---
+++ src/pages/Question/types/form.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+const questionSchema = z.object({
+	question: z.string(),
+	link: z.string().url(),
+	answerPlaceHolder: z.string(),
+	file: z.string(),
+	maxText: z.number(),
+});
+
+export const questionsListSchema = z.object({
+	questionList: z.array(questionSchema),
+});
+
+export type qustionListTypes = z.infer<typeof questionsListSchema>;
