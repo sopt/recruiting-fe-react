6bdd4f31dab4986d6d56f9b5a2a6057ad25a2672
kimkyuhong <bobo3766@naver.com>
2025-06-22 18:14:31 +0900

    feat: 질문 추가 버튼 구현

---
+++ src/pages/Question/components/QuestionList.tsx
@@ -0,0 +1,42 @@
+import { Add } from '@/assets/svg';
+import QuestionBox from '@/pages/Question/components/QuestionBox';
+import { useFieldArray, useFormContext } from 'react-hook-form';
+
+const QuestionList = () => {
+	const { control } = useFormContext();
+
+	const { fields: questionFileds, append } = useFieldArray({
+		control,
+		name: 'questionList',
+	});
+
+	const addQuestion = () => {
+		append({
+			question: '',
+			link: '',
+			answerPlaceHolder: '',
+			file: '',
+			maxText: 0,
+		});
+	};
+
+	return (
+		<>
+			<ul>
+				{questionFileds.map((questionInfo, index) => (
+					<QuestionBox key={index} index={index + 1} />
+				))}
+			</ul>
+			<button
+				type="button"
+				onClick={addQuestion}
+				className="flex flex-row gap-[0.4rem] mb-[3.2rem] px-[2rem] py-[1.2rem] rounded-[10px] bg-gray700 label_2_16_sb cursor-pointer"
+			>
+				<Add width={20} />
+				질문 추가하기
+			</button>
+		</>
+	);
+};
+
+export default QuestionList;
