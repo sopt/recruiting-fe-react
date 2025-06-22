d548e46bfa1e9452c1ed92788baac059c7854332
namdaeun <nde40345@gmail.com>
2025-06-22 18:01:09 +0900

    feat: 페이지 레이아웃 재설정

--- src/pages/PostGeneration/PostGeneration.tsx
+++ src/pages/PostGeneration/PostGeneration.tsx
@@ -1,34 +1,42 @@
-import { MOCK_GENERATION_DATA } from "@/constants/generation";
-import usePagination from "@/hooks/usePagination";
-import Pagination from "@/layout/Components/Pagination";
-import GenerationTable from "@/pages/PostGeneration/components/GenerationTable";
-import { useMemo } from "react";
+import Pagination from '@/components/Pagination';
+import YbObRadioGroup from '@/components/YbObRadioGroup';
+import { MOCK_GENERATION_DATA } from '@/constants/generation';
+import usePagination from '@/hooks/usePagination';
+
+import GenerationTable from '@/pages/PostGeneration/components/GenerationTable';
+
+import { Button } from '@sopt-makers/ui';
+import { useMemo } from 'react';
 
 const PostGeneration = () => {
-	const ITEMS_PER_PAGE = 10;
+  const ITEMS_PER_PAGE = 10;
 
-	const { currentPage, totalPages, handlePageChange } = usePagination({
-		totalItems: MOCK_GENERATION_DATA.length,
-		itemsPerPage: ITEMS_PER_PAGE,
-	});
+  const { currentPage, totalPages, handlePageChange } = usePagination({
+    totalItems: MOCK_GENERATION_DATA.length,
+    itemsPerPage: ITEMS_PER_PAGE,
+  });
 
-	const paginatedData = useMemo(() => {
-		const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
-		const endIndex = startIndex + ITEMS_PER_PAGE;
+  const paginatedData = useMemo(() => {
+    const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+    const endIndex = startIndex + ITEMS_PER_PAGE;
 
-		return MOCK_GENERATION_DATA.slice(startIndex, endIndex);
-	}, [currentPage]);
+    return MOCK_GENERATION_DATA.slice(startIndex, endIndex);
+  }, [currentPage]);
 
-	return (
-		<div>
-			<GenerationTable data={paginatedData} />
-			<Pagination
-				totalPages={totalPages}
-				currentPage={currentPage}
-				onPageChange={handlePageChange}
-			/>
-		</div>
-	);
+  return (
+    <div className="flex flex-col gap-[4.2rem] mt-[3.1rem] overflow-hidden">
+      <div className="flex justify-between pr-[12.4rem]">
+        <YbObRadioGroup />
+        <Button variant="fill">기수 추가</Button>
+      </div>
+      <GenerationTable data={paginatedData} />
+      <Pagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
+    </div>
+  );
 };
 
 export default PostGeneration;
--- src/pages/PostGeneration/components/GenerationTable.tsx
+++ src/pages/PostGeneration/components/GenerationTable.tsx
@@ -1,159 +1,161 @@
-import { Trash } from "@/assets/svg";
+import { Trash } from '@/assets/svg';
 
 interface Period {
-	start: string;
-	end: string;
+  start: string;
+  end: string;
 }
 
 interface GenerationTableProps {
-	data: {
-		generation: string;
-		name: string;
-		applicationPeriod: Period;
-		applicationResultPeriod: Period;
-		finalResultPeriod: Period;
-	}[];
+  data: {
+    generation: string;
+    name: string;
+    applicationPeriod: Period;
+    applicationResultPeriod: Period;
+    finalResultPeriod: Period;
+  }[];
 }
 
 const HEADER_BASE_STYLE =
-	"p-[1rem] text-gray100 body_3_14_m bg-gray700 border-gray600";
+  'p-[1rem] text-gray100 body_3_14_m bg-gray700 border-gray600';
 const CELL_BASE_STYLE =
-	"h-[6rem] text-center text-white body_3_14_m bg-transparent border-b-[1px] border-gray700 align-middle";
+  'h-[6rem] text-center text-white body_3_14_m bg-transparent border-b-[1px] border-gray700 align-middle';
 
 const GenerationTable = ({ data }: GenerationTableProps) => {
-	return (
-		<table className="w-[122.5rem]">
-			<thead>
-				<tr>
-					<th
-						className={`w-[11rem] rounded-tl-[1rem] border-r-[1px] border-gray600 ${HEADER_BASE_STYLE}`}
-					>
-						기수
-					</th>
-					<th
-						className={`w-[11rem] border-r-[1px] border-gray600 ${HEADER_BASE_STYLE}`}
-					>
-						이름
-					</th>
-					<th
-						className={`w-[31.5rem] border-r-[1px] border-gray600 ${HEADER_BASE_STYLE}`}
-					>
-						서류 지원 기간
-					</th>
-					<th
-						className={`w-[31.5rem] border-r-[1px] border-gray600 ${HEADER_BASE_STYLE}`}
-					>
-						서류 결과 확인 기간
-					</th>
-					<th
-						className={`w-[31.5rem] border-r-[1px] border-gray600 ${HEADER_BASE_STYLE}`}
-					>
-						최종 결과 확인 기간
-					</th>
-					<th className={`w-[5rem] rounded-tr-[1rem] ${HEADER_BASE_STYLE}`} />
-				</tr>
-			</thead>
-			<tbody>
-				{data.map((item, index) => (
-					<tr
-						key={index}
-						className="hover:bg-gray900 transition-colors duration-300"
-					>
-						<td className={`${CELL_BASE_STYLE} border-r-[1px]`}>
-							<div className="h-full flex items-center justify-center">
-								{item.generation}
-							</div>
-						</td>
-						<td className={`${CELL_BASE_STYLE} border-r-[1px]`}>
-							<div className="h-full flex items-center justify-center">
-								{item.name}
-							</div>
-						</td>
-						<td className={`${CELL_BASE_STYLE} border-r-[1px]`}>
-							<div className="h-full flex items-center justify-evenly gap-[0.5rem]">
-								<p>{item.applicationPeriod.start}</p>
-								<p>~</p>
-								<p>{item.applicationPeriod.end}</p>
-							</div>
-						</td>
-						<td className={`${CELL_BASE_STYLE} border-r-[1px]`}>
-							<div className="h-full flex items-center justify-evenly gap-[0.5rem]">
-								<p>{item.applicationResultPeriod.start}</p>
-								<p>~</p>
-								<p>{item.applicationResultPeriod.end}</p>
-							</div>
-						</td>
-						<td className={`${CELL_BASE_STYLE} border-r-[1px]`}>
-							<div className="h-full flex items-center justify-evenly gap-[0.5rem]">
-								<p>{item.finalResultPeriod.start}</p>
-								<p>~</p>
-								<p>{item.finalResultPeriod.end}</p>
-							</div>
-						</td>
-						<td className={CELL_BASE_STYLE}>
-							<div className="h-full flex items-center justify-center">
-								<button type="button" className="cursor-pointer">
-									<Trash width={22} className="stroke-white" />
-								</button>
-							</div>
-						</td>
-					</tr>
-				))}
-			</tbody>
-		</table>
-	);
+  return (
+    <div className="w-full overflow-x-auto scroll-smooth scrollbar-hide pr-[12.4rem]">
+      <table className="w-[122.5rem]">
+        <thead>
+          <tr>
+            <th
+              className={`w-[11rem] rounded-tl-[1rem] border-r-[1px] border-gray600 ${HEADER_BASE_STYLE}`}
+            >
+              기수
+            </th>
+            <th
+              className={`w-[11rem] border-r-[1px] border-gray600 ${HEADER_BASE_STYLE}`}
+            >
+              이름
+            </th>
+            <th
+              className={`w-[31.5rem] border-r-[1px] border-gray600 ${HEADER_BASE_STYLE}`}
+            >
+              서류 지원 기간
+            </th>
+            <th
+              className={`w-[31.5rem] border-r-[1px] border-gray600 ${HEADER_BASE_STYLE}`}
+            >
+              서류 결과 확인 기간
+            </th>
+            <th
+              className={`w-[31.5rem] border-r-[1px] border-gray600 ${HEADER_BASE_STYLE}`}
+            >
+              최종 결과 확인 기간
+            </th>
+            <th className={`w-[5rem] rounded-tr-[1rem] ${HEADER_BASE_STYLE}`} />
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr
+              key={index}
+              className="hover:bg-gray900 transition-colors duration-300"
+            >
+              <td className={`${CELL_BASE_STYLE} border-r-[1px]`}>
+                <div className="h-full flex items-center justify-center">
+                  {item.generation}
+                </div>
+              </td>
+              <td className={`${CELL_BASE_STYLE} border-r-[1px]`}>
+                <div className="h-full flex items-center justify-center">
+                  {item.name}
+                </div>
+              </td>
+              <td className={`${CELL_BASE_STYLE} border-r-[1px]`}>
+                <div className="h-full flex items-center justify-evenly gap-[0.5rem]">
+                  <p>{item.applicationPeriod.start}</p>
+                  <p>~</p>
+                  <p>{item.applicationPeriod.end}</p>
+                </div>
+              </td>
+              <td className={`${CELL_BASE_STYLE} border-r-[1px]`}>
+                <div className="h-full flex items-center justify-evenly gap-[0.5rem]">
+                  <p>{item.applicationResultPeriod.start}</p>
+                  <p>~</p>
+                  <p>{item.applicationResultPeriod.end}</p>
+                </div>
+              </td>
+              <td className={`${CELL_BASE_STYLE} border-r-[1px]`}>
+                <div className="h-full flex items-center justify-evenly gap-[0.5rem]">
+                  <p>{item.finalResultPeriod.start}</p>
+                  <p>~</p>
+                  <p>{item.finalResultPeriod.end}</p>
+                </div>
+              </td>
+              <td className={CELL_BASE_STYLE}>
+                <div className="h-full flex items-center justify-center">
+                  <button type="button" className="cursor-pointer">
+                    <Trash width={22} className="stroke-white" />
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default GenerationTable;
 
 // Mock data 추가
 export const mockGenerationData = [
-	{
-		generation: "2025",
-		name: "AT SOPT",
-		applicationPeriod: {
-			start: "2024.12.01",
-			end: "2024.12.31",
-		},
-		applicationResultPeriod: {
-			start: "2025.01.15",
-			end: "2025.01.20",
-		},
-		finalResultPeriod: {
-			start: "2025.02.01",
-			end: "2025.02.05",
-		},
-	},
-	{
-		generation: "2024",
-		name: "AT SOPT",
-		applicationPeriod: {
-			start: "2023.12.01",
-			end: "2023.12.31",
-		},
-		applicationResultPeriod: {
-			start: "2024.01.15",
-			end: "2024.01.20",
-		},
-		finalResultPeriod: {
-			start: "2024.02.01",
-			end: "2024.02.05",
-		},
-	},
-	{
-		generation: "2023",
-		name: "AT SOPT",
-		applicationPeriod: {
-			start: "2022.12.01",
-			end: "2022.12.31",
-		},
-		applicationResultPeriod: {
-			start: "2023.01.15",
-			end: "2023.01.20",
-		},
-		finalResultPeriod: {
-			start: "2023.02.01",
-			end: "2023.02.05",
-		},
-	},
+  {
+    generation: '2025',
+    name: 'AT SOPT',
+    applicationPeriod: {
+      start: '2024.12.01',
+      end: '2024.12.31',
+    },
+    applicationResultPeriod: {
+      start: '2025.01.15',
+      end: '2025.01.20',
+    },
+    finalResultPeriod: {
+      start: '2025.02.01',
+      end: '2025.02.05',
+    },
+  },
+  {
+    generation: '2024',
+    name: 'AT SOPT',
+    applicationPeriod: {
+      start: '2023.12.01',
+      end: '2023.12.31',
+    },
+    applicationResultPeriod: {
+      start: '2024.01.15',
+      end: '2024.01.20',
+    },
+    finalResultPeriod: {
+      start: '2024.02.01',
+      end: '2024.02.05',
+    },
+  },
+  {
+    generation: '2023',
+    name: 'AT SOPT',
+    applicationPeriod: {
+      start: '2022.12.01',
+      end: '2022.12.31',
+    },
+    applicationResultPeriod: {
+      start: '2023.01.15',
+      end: '2023.01.20',
+    },
+    finalResultPeriod: {
+      start: '2023.02.01',
+      end: '2023.02.05',
+    },
+  },
 ];
