a4464073ca1e383550a0790e220ac8141d3330ee
kimkyuhong <bobo3766@naver.com>
2025-06-23 15:50:53 +0900

    chore: lint 오류 2차 해결

--- src/assets/svg/Add.tsx
+++ src/assets/svg/Add.tsx
@@ -1,16 +1,16 @@
 import type { SVGProps } from 'react';
 const SvgAdd = (props: SVGProps<SVGSVGElement>) => (
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		fill="none"
-		viewBox="0 0 16 16"
-		{...props}
-	>
-		<path
-			stroke="#9D9DA4"
-			strokeLinecap="round"
-			d="M2.602 8.273h11M8.102 13.773v-11"
-		/>
-	</svg>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 16 16"
+    {...props}
+  >
+    <path
+      stroke="#9D9DA4"
+      strokeLinecap="round"
+      d="M2.602 8.273h11M8.102 13.773v-11"
+    />
+  </svg>
 );
 export default SvgAdd;
