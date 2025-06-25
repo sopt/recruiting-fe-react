dc61470af61521e8fb868aab2cd12f914b42664c
namdaeun <nde40345@gmail.com>
2025-06-26 02:22:16 +0900

    feat: 모달 틀 구현

---
+++ src/pages/PostGeneration/components/Calendar.tsx
@@ -0,0 +1,78 @@
+import dayjs from 'dayjs';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import 'dayjs/locale/ko';
+import {
+  type Dispatch,
+  type MouseEvent,
+  type SetStateAction,
+  useState,
+} from 'react';
+import '@/styles/calendar.css';
+import type { Value } from 'react-calendar/dist/shared/types.js';
+
+interface Props {
+  selectedDate: string[] | string | null;
+  setSelectedDate: Dispatch<SetStateAction<string[]>>;
+  selectedDateFieldName: string;
+  error?: string;
+  dateType?: 'startDate' | 'endDate' | 'singleSelect' | 'range';
+  onDateSelect?: (date: string) => void;
+  onClose?: () => void;
+}
+
+const formatCalendarDate = (date: Date) => {
+  return dayjs(date).format('YYYY.MM.DD');
+};
+
+const WEEKDAYS = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+
+const CalendarInputForm = ({
+  selectedDate,
+  setSelectedDate,
+  dateType = 'range',
+  onClose,
+}: Props) => {
+  const [isOpen, setIsOpen] = useState(true);
+  const [rangeValue, setRangeValue] = useState<[Date | null, Date | null]>([
+    null,
+    null,
+  ]);
+
+  const handleDateChange = (
+    value: Date | [Date, Date],
+    event: MouseEvent<HTMLButtonElement>,
+  ) => {
+    if (Array.isArray(value)) {
+      setRangeValue(value);
+
+      const [start, end] = value;
+      if (start && end) {
+        const formatted = [formatCalendarDate(start), formatCalendarDate(end)];
+        setSelectedDate(formatted);
+        onClose?.();
+      }
+    }
+  };
+  const CalendarComponent = () => (
+    <Calendar
+      value={rangeValue}
+      onChange={(value: Value, event: MouseEvent<HTMLButtonElement>) =>
+        handleDateChange(value as [Date, Date], event)
+      }
+      selectRange
+      formatDay={(locale, date) => dayjs(date).format('D')}
+      formatShortWeekday={(locale, date) => WEEKDAYS[date.getDay()] ?? ''}
+      showNeighboringMonth={false}
+      next2Label={null}
+      prev2Label={null}
+      minDetail="month"
+      maxDetail="month"
+      calendarType="gregory"
+    />
+  );
+
+  return <CalendarComponent />;
+};
+
+export default CalendarInputForm;
---
+++ src/pages/PostGeneration/components/PeriodCalendar.tsx
@@ -0,0 +1,112 @@
+import CalendarInputForm from '@/pages/PostGeneration/components/Calendar';
+import { IconCalendar } from '@sopt-makers/icons';
+import { useEffect, useRef, useState } from 'react';
+
+interface Props {
+  label: string;
+  required?: boolean;
+}
+
+const PeriodCalendar = ({ label, required = false }: Props) => {
+  const [isCalendarOpen, setIsCalendarOpen] = useState(false);
+  const [selectedDateRange, setSelectedDateRange] = useState<string[]>([
+    '',
+    '',
+  ]);
+  const [activeInput, setActiveInput] = useState<'start' | 'end' | null>(null);
+  const calendarRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        calendarRef.current &&
+        !calendarRef.current.contains(event.target as Node)
+      ) {
+        setIsCalendarOpen(false);
+        setActiveInput(null);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isCalendarOpen]);
+
+  const handleInputClick = (target: 'start' | 'end') => {
+    setActiveInput(target);
+    setIsCalendarOpen(true);
+  };
+
+  const handleDateSelect = (date: string) => {
+    if (activeInput === 'start') {
+      const end = selectedDateRange[1];
+      if (end && date > end) {
+        setSelectedDateRange([end, date]);
+      } else {
+        setSelectedDateRange([date, end]);
+      }
+    } else if (activeInput === 'end') {
+      const start = selectedDateRange[0];
+      if (start && date < start) {
+        setSelectedDateRange([date, start]);
+      } else {
+        setSelectedDateRange([start, date]);
+      }
+    }
+  };
+  return (
+    <label className="flex flex-col gap-[0.8rem] text-gray-10 text-[1.4rem] font-semibold">
+      <span className="flex text-white items-center label_3_14_sb gap-[0.4rem]">
+        {label}
+        {required && <span className="text-secondary">*</span>}
+      </span>
+      <div className="flex gap-[0.8rem] relative" ref={calendarRef}>
+        <div
+          className="flex w-full px-4 py-[13px] text-gray10 bg-gray700 rounded-[10px] justify-between cursor-pointer"
+          onClick={() => handleInputClick('start')}
+          onKeyUp={(e) => e.key === 'Enter' && handleInputClick('start')}
+        >
+          <input
+            type="text"
+            value={selectedDateRange[0]}
+            placeholder="YYYY.MM.DD"
+            readOnly
+            className="w-4/5 flex items-center cursor-pointer text-[1.6rem] font-medium bg-transparent border-none placeholder:text-gray500 focus:outline-none"
+          />
+          <IconCalendar style={{ width: '24' }} />
+        </div>
+
+        <p className="text-gray-10 text-[1.4rem] font-semibold">-</p>
+
+        <div
+          className="flex w-full px-4 py-[13px] text-gray10 bg-gray700 rounded-[10px] justify-between cursor-pointer"
+          onClick={() => handleInputClick('end')}
+          onKeyUp={(e) => e.key === 'Enter' && handleInputClick('end')}
+        >
+          <input
+            type="text"
+            value={selectedDateRange[1]}
+            placeholder="YYYY.MM.DD"
+            readOnly
+            className="relative w-4/5 flex items-center cursor-pointer text-[1.6rem] font-medium bg-transparent border-none placeholder:text-gray500 focus:outline-none"
+          />
+          <IconCalendar style={{ width: '24' }} />
+        </div>
+        {isCalendarOpen && (
+          <div className="absolute z-[9999] top-full left-0 mt-2 bg-gray700 text-gray10 p-4 w-[320px] rounded-2xl shadow-lg">
+            <CalendarInputForm
+              dateType="range"
+              selectedDate={selectedDateRange}
+              setSelectedDate={setSelectedDateRange}
+              selectedDateFieldName="date-range"
+              onDateSelect={handleDateSelect}
+              onClose={() => setIsCalendarOpen(false)}
+            />
+          </div>
+        )}
+      </div>
+    </label>
+  );
+};
+
+export default PeriodCalendar;
