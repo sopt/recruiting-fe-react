6c102d807165845130622a75dcf326a387c6342c
namdaeun <nde40345@gmail.com>
2025-06-26 23:00:51 +0900

    feat: 폼 관리 코드 추가

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,21 +1,38 @@
 import { Logo } from '@/assets/svg';
 import { Button, TextField } from '@sopt-makers/ui';
+import { type SubmitHandler, useForm } from 'react-hook-form';
+
+type LoginForm = {
+  email: string;
+  password: string;
+};
 
 const Login = () => {
+  const { handleSubmit, register } = useForm<LoginForm>();
+
+  const onSubmit: SubmitHandler<LoginForm> = () => {
+    // TODO: 로그인 POST 요청
+  };
+
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen gap-[1.2rem]">
       <div className="flex flex-col items-center justify-center gap-[1.2rem]">
         <Logo width={172} />
         <h1 className="heading_5_20_b text-gray10">Recruit Admin</h1>
       </div>
-      <form className="flex flex-col gap-[1.6rem] w-[40rem]">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-[1.6rem] w-[40rem]"
+      >
         <TextField
           labelText="이메일"
           placeholder="이메일 주소를 입력해주세요."
+          {...register('email')}
         />
         <TextField
           labelText="비밀번호"
           placeholder="비밀번호를 입력해주세요."
+          {...register('password')}
         />
         <Button type="submit" className="mt-[3.2rem]">
           로그인
