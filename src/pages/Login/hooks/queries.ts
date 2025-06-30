eafd1795701276a2906bbe0c1b3e732a2d4fc7f3
namdaeun <nde40345@gmail.com>
2025-07-01 01:07:26 +0900

    feat: 로그인 성공시 지원서 페이지로 라우팅

--- src/pages/Login/hooks/queries.ts
+++ src/pages/Login/hooks/queries.ts
@@ -1,14 +1,20 @@
 import type { LoginResponse } from '@/pages/Login/types';
+import { ROUTES_CONFIG } from '@/routes/routeConfig';
 import { useMutation } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
 import { postLogin } from '../apis/postLogin';
 
 export const usePostLogin = () => {
+  const navigate = useNavigate();
+
   return useMutation({
     mutationFn: ({ email, password }: { email: string; password: string }) =>
       postLogin(email, password),
     onSuccess: (data: LoginResponse) => {
       localStorage.setItem('accessToken', data.data.token);
       localStorage.setItem('role', data.data.role);
+
+      navigate(ROUTES_CONFIG.application.path);
     },
   });
 };
