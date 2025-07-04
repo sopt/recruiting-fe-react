88aae831e37f82f6a88c0adf82c10de148d33b9d
namdaeun <nde40345@gmail.com>
2025-07-04 22:48:23 +1000

    feat: 헤더에 토큰 추가

--- src/pages/PostGeneration/apis/deleteGeneration.ts
+++ src/pages/PostGeneration/apis/deleteGeneration.ts
@@ -4,6 +4,9 @@ export const deleteGeneration = (seasonId: number) => {
   const response = api
     .delete('api/v2/recruiting-season', {
       json: { seasonId },
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
+      },
     })
     .json();
 
