2d4dcf8b5c40155666a88a18b1d6c5df8c9a680f
namdaeun <nde40345@gmail.com>
2025-07-04 23:34:47 +1000

    chore: tokenApi 사용

--- src/pages/PostGeneration/apis/deleteGeneration.ts
+++ src/pages/PostGeneration/apis/deleteGeneration.ts
@@ -1,12 +1,9 @@
-import { api } from '@/apis/api';
+import { tokenApi } from '@/apis/api';
 
 export const deleteGeneration = (seasonId: number) => {
-  const response = api
+  const response = tokenApi
     .delete('api/v2/recruiting-season', {
       json: { seasonId },
-      headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
-      },
     })
     .json();
 
--- src/pages/PostGeneration/apis/postGeneration.ts
+++ src/pages/PostGeneration/apis/postGeneration.ts
@@ -1,16 +1,13 @@
-import { api } from '@/apis/api';
+import { tokenApi } from '@/apis/api';
 import type {
   PostGenerationRequest,
   PostGenerationResponse,
 } from '@/pages/PostGeneration/types';
 
 export const postGeneration = async (season: PostGenerationRequest) => {
-  const response = await api
+  const response = await tokenApi
     .post<PostGenerationResponse>('api/v2/recruiting-season', {
       json: season,
-      headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
-      },
     })
     .json();
 
