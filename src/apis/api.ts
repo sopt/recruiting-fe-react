42f56b02a45d78850616f14cf793f5b1d00d99fa
namdaeun <nde40345@gmail.com>
2025-07-06 23:07:56 +1000

    fix: extend로 변경

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -5,8 +5,7 @@ export const api = ky.create({
   prefixUrl: import.meta.env.VITE_BASE_URL,
 });
 
-export const tokenApi = ky.create({
-  prefixUrl: import.meta.env.VITE_BASE_URL,
+export const tokenApi = api.extend({
   headers: {
     Authorization: `Bearer ${getAccessToken()}`,
   },
