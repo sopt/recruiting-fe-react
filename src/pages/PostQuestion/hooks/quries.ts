b2733be9f56cddc2a85f7b4c9c659611e5c97c0e
kimkyuhong <bobo3766@naver.com>
2025-07-06 20:18:34 +0900

    feat: 임시저장 성공 토스터 띄우기

--- src/pages/PostQuestion/hooks/quries.ts
+++ src/pages/PostQuestion/hooks/quries.ts
@@ -1,11 +1,15 @@
 import { getQuestionList } from '@/pages/PostQuestion/apis/getQuestionList';
 import { postQuestionsSave } from '@/pages/PostQuestion/apis/postQuestionsSave';
 import type { GROUP, QuestionSubmitRequest } from '@/pages/PostQuestion/types';
+import { useToast } from '@sopt-makers/ui';
 import { useMutation, useQuery } from '@tanstack/react-query';
 
 export const usePostQuestionsSave = () => {
+  const { open } = useToast();
   return useMutation({
     mutationFn: (data: QuestionSubmitRequest) => postQuestionsSave(data),
+    onSuccess: () =>
+      open({ icon: 'success', content: '임시저장이 완료되었어요.' }),
   });
 };
 
