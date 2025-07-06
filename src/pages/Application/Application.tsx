4bc18738b319b847ddd24d7080c9c08c36900e0a
namdaeun <nde40345@gmail.com>
2025-07-06 23:16:03 +1000

    refactor: 키에 대한 상태 업데이트 함수 분리

--- src/pages/Application/Application.tsx
+++ src/pages/Application/Application.tsx
@@ -11,36 +11,61 @@ import type { Group } from '@/pages/Question/types';
 import { Tab } from '@sopt-makers/ui';
 import { useMemo, useState } from 'react';
 
-const LIMIT = 5;
+const LIST_LIMIT = 10;
+const PAGE_LIMIT = 5;
+
+interface ApplicantState {
+  season: string;
+  group: Group;
+  isEvaluated: boolean;
+  isDontRead: boolean;
+  isPassedOnly: boolean;
+  selectedPart: PartType;
+}
 
 const Application = () => {
-  const [season, setSeason] = useState('36기');
-  const [group, setGroup] = useState<Group>('YB');
-  const [isEvaluated, setIsEvaluated] = useState(false);
-  const [isDontRead, setIsDontRead] = useState(false);
-  const [isPassedOnly, setIsPassedOnly] = useState(false);
-  const [selectedPart, setSelectedPart] = useState<PartType>('WEB');
+  const [applicantInfo, setApplicantInfo] = useState<ApplicantState>({
+    season: '36기',
+    group: 'YB',
+    isEvaluated: false,
+    isDontRead: false,
+    isPassedOnly: false,
+    selectedPart: 'ALL',
+  });
+
+  const updateState = <K extends keyof ApplicantState>(key: K) => {
+    return (
+      value:
+        | ApplicantState[K]
+        | ((prev: ApplicantState[K]) => ApplicantState[K]),
+    ) => {
+      setApplicantInfo((prev) => ({
+        ...prev,
+        [key]: typeof value === 'function' ? value(prev[key]) : value,
+      }));
+    };
+  };
 
   const { data } = useGetApplicantList({
-    season: Number(season.split('기')[0]),
-    group,
-    part: selectedPart,
+    season: Number(applicantInfo.season.split('기')[0]),
+    group: applicantInfo.group,
+    part: applicantInfo.selectedPart,
     offset: 0,
-    limit: 10,
+    limit: LIST_LIMIT,
     minRate: 0,
-    hideEvaluated: isEvaluated,
-    hideDontRead: isDontRead,
-    checkInterviewPass: isPassedOnly,
+    hideEvaluated: applicantInfo.isEvaluated,
+    hideDontRead: applicantInfo.isDontRead,
+    checkInterviewPass: applicantInfo.isPassedOnly,
   });
 
   const { currentPage, totalPages, handlePageChange } = usePagination({
     totalItems: data?.data.length ?? 0,
-    limit: LIMIT,
+    limit: PAGE_LIMIT,
   });
 
   const paginatedData = useMemo(() => {
-    const startIndex = (currentPage - 1) * LIMIT;
-    const endIndex = startIndex + LIMIT;
+    const startIndex = (currentPage - 1) * PAGE_LIMIT;
+    const endIndex = startIndex + PAGE_LIMIT;
 
     return data?.data.slice(startIndex, endIndex) ?? [];
   }, [currentPage]);
@@ -48,23 +73,23 @@ const Application = () => {
   return (
     <div className="flex flex-col gap-[4.4rem]">
       <Filter
-        season={season}
-        setSeason={setSeason}
-        group={group}
-        setGroup={setGroup}
-        isEvaluated={isEvaluated}
-        isDontRead={isDontRead}
-        isPassedOnly={isPassedOnly}
-        setIsEvaluated={setIsEvaluated}
-        setIsDontRead={setIsDontRead}
-        setIsPassedOnly={setIsPassedOnly}
+        season={applicantInfo.season}
+        setSeason={updateState('season')}
+        group={applicantInfo.group}
+        setGroup={updateState('group')}
+        isEvaluated={applicantInfo.isEvaluated}
+        isDontRead={applicantInfo.isDontRead}
+        isPassedOnly={applicantInfo.isPassedOnly}
+        setIsEvaluated={updateState('isEvaluated')}
+        setIsDontRead={updateState('isDontRead')}
+        setIsPassedOnly={updateState('isPassedOnly')}
       />
       <Tab
         style="primary"
         size="md"
         tabItems={Object.keys(PART_TRANSLATOR) as PartType[]}
         translator={PART_TRANSLATOR}
-        onChange={(part: PartType) => setSelectedPart(part)}
+        onChange={updateState('selectedPart')}
       />
       <hr className="border-gray800 mt-[-4.7rem] w-[98rem]" />
       <ApplicationTable data={paginatedData} />
