f18dc0b05558cb32b990070e70b5b6e3c17e92bd
kimkyuhong <bobo3766@naver.com>
2025-07-07 21:07:46 +0900

    feat: zod에 isLink, isFile 추가

--- src/pages/PostQuestion/types/form.ts
+++ src/pages/PostQuestion/types/form.ts
@@ -1,13 +1,25 @@
 import { z } from 'zod/v4';
 
-const questionSchema = z.object({
-  id: z.number().optional(),
-  question: z.string(),
-  link: z.url(),
-  placeholder: z.string(),
-  isFile: z.boolean(),
-  charLimit: z.number(),
-});
+const questionSchema = z
+  .object({
+    id: z.number().optional(),
+    question: z.string(),
+    isLink: z.boolean(),
+    link: z.url().optional(),
+    placeholder: z.string(),
+    isFile: z.boolean(),
+    charLimit: z.number(),
+  })
+  .check((ctx) => {
+    if (ctx.value.isLink && !ctx.value.link) {
+      ctx.issues.push({
+        message: '필수 입력란입니다.',
+        input: ctx.value,
+        path: ['link'],
+        code: 'custom',
+      });
+    }
+  });
 
 export const questionsListSchema = z.object({
   questionList: z.array(questionSchema),
