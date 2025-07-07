da4230d2e9229b9e32fa3c5aae1b6dbf4ff254a1
kimkyuhong <bobo3766@naver.com>
2025-07-07 19:44:20 +0900

    feat: token api로 변경

--- src/pages/PostQuestion/apis/getQuestionList.ts
+++ src/pages/PostQuestion/apis/getQuestionList.ts
@@ -1,15 +1,12 @@
-import { api } from '@/apis/api';
+import { tokenApi } from '@/apis/api';
 import type { GROUP } from '@/pages/PostQuestion/types';
 
 export const getQuestionList = async (season: number, group: GROUP) => {
-  const response = await api
+  const response = await tokenApi
     .get('api/v2/recruiting-question/admin/list', {
       searchParams: {
         season: season,
         group: group,
-      },
-      headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
       },
     })
     .json();
--- src/pages/PostQuestion/apis/postQuestionsSave.ts
+++ src/pages/PostQuestion/apis/postQuestionsSave.ts
@@ -1,13 +1,10 @@
-import { api } from '@/apis/api';
+import { tokenApi } from '@/apis/api';
 import type { QuestionSubmitRequest } from '@/pages/PostQuestion/types';
 
 export const postQuestionsSave = async (data: QuestionSubmitRequest) => {
-  const response = await api
+  const response = await tokenApi
     .post('api/v2/recruiting-question/save', {
       json: { ...data },
-      headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
-      },
     })
     .json();
 
