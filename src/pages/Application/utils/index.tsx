bd4ebdf0c74e8b1925ea034e618402f3805bd2c4
namdaeun <nde40345@gmail.com>
2025-07-09 00:29:36 +1000

    chore: 코드 리뷰 반영

--- src/pages/Application/utils/index.tsx
+++ src/pages/Application/utils/index.tsx
@@ -51,8 +51,3 @@ export const convertStatusToPassInfo = (
       return { applicationPass: null, finalPass: null };
   }
 };
-
-export const isNumberValue = (value: string): boolean => {
-  // biome-ignore lint/performance/useTopLevelRegex: 숫자 형식 검사
-  return value === '' || /^\d*\.?\d*$/.test(value);
-};
