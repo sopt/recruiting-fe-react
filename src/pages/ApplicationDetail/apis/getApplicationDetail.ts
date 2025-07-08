c317b5a73a25eaeab15ae6269e6b8b3e0a0ab544
kimkyuhong <bobo3766@naver.com>
2025-07-08 15:39:09 +0900

    feat: 코드리뷰 반영

--- src/pages/ApplicationDetail/apis/getApplicationDetail.ts
+++ src/pages/ApplicationDetail/apis/getApplicationDetail.ts
@@ -1,16 +1,10 @@
-import { api } from '@/apis/api';
+import { tokenApi } from '@/apis/api';
 import type { ApplicationDetailResponse } from '@/pages/ApplicationDetail/types';
 
 export const getApplicationDetail = async (applicantId: number) => {
-  const res = await api.get(
-    `api/v2/recruiting-admin/applicant/detail?applicantId=${applicantId}`,
-    {
-      headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
-      },
-    },
-  );
-  const data = await res.json<ApplicationDetailResponse>();
+  const data = await tokenApi
+    .get(`api/v2/recruiting-admin/applicant/detail?applicantId=${applicantId}`)
+    .json<ApplicationDetailResponse>();
 
   return data.data;
 };
