1a783b91c17d35fc084746699ee5dcc447bb8d73
kimkyuhong <bobo3766@naver.com>
2025-07-14 10:32:19 +0900

    feat: 로그아웃 버튼 상단 고정

--- src/layout/Components/LogoutButton.tsx
+++ src/layout/Components/LogoutButton.tsx
@@ -2,7 +2,7 @@ const LogoutButton = () => {
   return (
     <button
       type="button"
-      className="absolute top-[2.1rem] right-[6.5rem] px-[2.4rem] py-[0.7rem] rounded-[19px] bg-gray800 text-gray200 text-[1.6rem] font-semibold leading-[2.4rem] tracking-[-0.24px]"
+      className="fixed z-5 top-[2.1rem] right-[6.5rem] px-[2.4rem] py-[0.7rem] rounded-[19px] bg-gray800 text-gray200 text-[1.6rem] font-semibold leading-[2.4rem] tracking-[-0.24px] cursor-pointer"
     >
       로그아웃
     </button>
