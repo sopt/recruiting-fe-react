22c1c6b8d2fbdce645386a537c2f4c39caf7e9f9
namdaeun <nde40345@gmail.com>
2025-07-22 00:49:46 +0800

    chore: 타입 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,8 @@ export enum ExecutiveType {
   PRESIDENT = '회장',
   VICE_PRESIDENT = '부회장',
   MEDIA = '미디어',
+  OPERATION = '운영',
+  FINANCE = '총무',
   PLAN = '기획',
   DESIGN = '디자인',
   SERVER = '서버',
