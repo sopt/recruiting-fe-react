65657327bc2cf87107333709ff624a8070396b37
namdaeun <nde40345@gmail.com>
2025-07-22 00:48:10 +0800

    refactor: 공통 유틸 함수 분리

--- src/pages/ApplicationDetail/utils/index.ts
+++
@@ -1,7 +0,0 @@
-export const getDontReadMessage = (list: string[]) => {
-  return `${list.join(', ')}이(가) 읽지 말라고 선택했어요.`;
-};
-
-export const getEvalutionCompleteMessage = (list: string[]) => {
-  return `${list.join(', ')}이(가) 평가를 완료했어요.`;
-};
---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export enum ExecutiveType {
+  PRESIDENT = '회장',
+  VICE_PRESIDENT = '부회장',
+  MEDIA = '미디어',
+  PLAN = '기획',
+  DESIGN = '디자인',
+  SERVER = '서버',
+  WEB = '웹',
+  IOS = 'iOS',
+  ANDROID = '안드로이드',
+}
