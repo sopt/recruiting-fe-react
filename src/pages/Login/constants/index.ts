23e0368a7df768ad7ea61df72ac4f0902851f1fd
namdaeun <nde40345@gmail.com>
2025-07-22 23:43:26 +0800

    feat: 로그인 에러 케이스 추가

---
+++ src/pages/Login/constants/index.ts
@@ -0,0 +1,5 @@
+export const NOT_FOUND_ERROR_CODE = '400';
+export const NOT_FOUND_ERROR = '잘못된 이메일 혹은 비밀번호입니다.';
+export const SERVER_ERROR = '로그인에 실패했습니다.';
+export const EMAIL_ERROR = '이메일 형식이 올바르지 않습니다.';
+export const PASSWORD_ERROR = '비밀번호를 입력해주세요.';
