aa2392ffed3ed8bd94c6683e9a11cde82a4b464b
namdaeun <nde40345@gmail.com>
2025-07-23 21:42:30 +0800

    fix: 드롭다운 리스트 수정

--- src/pages/Application/constants/index.ts
+++ src/pages/Application/constants/index.ts
@@ -24,8 +24,8 @@ export const EXECUTIVE_TRANSLATOR = {
 };
 
 export const CHIP_STATUS = {
+  '최종 합격': 'text-success bg-blueAlpha100 border-blueAlpha600',
+  '확인 전': 'text-white bg-grayAlpha100 border-gray400',
   '서류 합격': 'text-information bg-greenAlpha100 border-greenAlpha400',
   불합격: 'text-error bg-redAlpha100 border-redAlpha600',
-  '확인 전': 'text-gray100 bg-grayAlpha100 border-gray400',
-  '최종 합격': 'text-success bg-blueAlpha100 border-blueAlpha600',
 };
