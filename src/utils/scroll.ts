953ac3f5a91d52d60f65ff7e9c06650d2b766237
namdaeun <nde40345@gmail.com>
2025-07-26 22:36:36 +0900

    feat: 스크롤 유틸 함수 생성

---
+++ src/utils/scroll.ts
@@ -0,0 +1,17 @@
+import type { RefObject } from 'react';
+
+export const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+};
+
+export const scrollToLeft = (elementRef?: RefObject<HTMLElement>) => {
+  if (elementRef?.current) {
+    elementRef.current.scrollTo({
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
+};
