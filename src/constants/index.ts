c75c99e549d63db9b7cd09f9af934213e43b8969
namdaeun <nde40345@gmail.com>
2025-08-13 19:10:09 +0900

    fix: preview도 확인 가능하게 도메인 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,4 +2,7 @@ export const ACCESS_TOKEN = 'accessToken';
 export const ROLE = 'role';
 
 export const SOPT_DOMAIN = 'recruit-admin.sopt';
-export const IS_SOPT = window.location.hostname.includes(SOPT_DOMAIN);
+export const SOPT_PREVIEW_DOMAIN = 'recruiting-fe-react-sopt';
+export const IS_SOPT = window.location.hostname.includes(
+  SOPT_DOMAIN || SOPT_PREVIEW_DOMAIN,
+);
