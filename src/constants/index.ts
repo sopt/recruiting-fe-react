f3f9feb2e9b9da94d4592427dd912a89a837a393
namdaeun <nde40345@gmail.com>
2025-08-13 18:57:20 +0900

    fix: includes로 변경

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const ACCESS_TOKEN = 'accessToken';
 export const ROLE = 'role';
 
-export const SOPT_DOMAIN = 'recruit-admin.sopt.org';
-export const IS_SOPT = window.location.hostname === SOPT_DOMAIN;
+export const SOPT_DOMAIN = 'recruit-admin.sopt';
+export const IS_SOPT = window.location.hostname.includes(SOPT_DOMAIN);
