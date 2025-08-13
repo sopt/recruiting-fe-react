c40938705999a81d40d8fd729b0076dc7a4dd98b
namdaeun <nde40345@gmail.com>
2025-08-13 19:13:42 +0900

    fix: includes문 수정

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,6 +3,6 @@ export const ROLE = 'role';
 
 export const SOPT_DOMAIN = 'recruit-admin.sopt';
 export const SOPT_PREVIEW_DOMAIN = 'recruiting-fe-react-sopt';
-export const IS_SOPT = window.location.hostname.includes(
-  SOPT_DOMAIN || SOPT_PREVIEW_DOMAIN,
-);
+export const IS_SOPT =
+  window.location.hostname.includes(SOPT_DOMAIN) ||
+  window.location.hostname.includes(SOPT_PREVIEW_DOMAIN);
