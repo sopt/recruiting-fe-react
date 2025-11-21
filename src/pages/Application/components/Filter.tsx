61095142ae2eb97924c6293278a2f88ab7eeafc9
namdaeun <nde40345@gmail.com>
2025-11-21 23:47:47 +0900

    feat: 검색 필드 및 합격 여부 select 추가

--- src/pages/Application/components/Filter.tsx
+++ src/pages/Application/components/Filter.tsx
@@ -1,4 +1,4 @@
-import { SelectV2, Toggle } from '@sopt-makers/ui';
+import { SelectV2, TextField, Toggle } from '@sopt-makers/ui';
 import YbObRadioGroup from '@/components/YbObRadioGroup';
 import type { ApplicantState } from '@/pages/Application/\btypes';
 import type { GetGenerationResponse } from '@/pages/PostGeneration/types';
@@ -10,6 +10,13 @@ interface FilterProps {
     info: ApplicantState | ((prev: ApplicantState) => ApplicantState)
   ) => void;
 }
+
+const STATUS_OPTIONS = [
+  { label: '최종 합격', value: 'FINAL_PASS' },
+  { label: '서류 합격', value: 'INTERVIEW_PASS' },
+  { label: '불합격', value: 'FAIL' },
+  { label: '확인 전', value: 'NOT_EVALUATED' },
+];
 
 const Filter = ({
   generationData,
@@ -63,7 +70,49 @@ const Filter = ({
       </div>
       <div className="flex flex-col gap-[0.8rem]">
         <div className="flex gap-[2.4rem]">
-          <div className="flex items-center gap-[0.8rem]">
+          <div className="flex flex-col gap-[0.8rem]">
+            <span className="flex body_3_14_r text-gray100">지원자 검색</span>
+            <TextField
+              placeholder="지원자 성명 입력"
+              value={''}
+              onChange={(e) =>
+                setApplicantInfo((prev) => ({
+                  ...prev,
+                  search: e.target.value,
+                }))
+              }
+            />
+          </div>
+          <div className="flex flex-col gap-[0.8rem]">
+            <span className="flex body_3_14_r text-gray100">합격 여부</span>
+            <SelectV2.Root visibleOptions={7} type="text" multiple>
+              <SelectV2.Trigger>
+                <div>
+                  <SelectV2.TriggerContent placeholder={'전체'} />
+                </div>
+              </SelectV2.Trigger>
+              {STATUS_OPTIONS.length > 0 ? (
+                <SelectV2.Menu>
+                  {STATUS_OPTIONS.map((option) => (
+                    <SelectV2.MenuItem
+                      key={option.value}
+                      option={{
+                        label: option.label,
+                        value: option.value,
+                      }}
+                      onClick={() =>
+                        setApplicantInfo((prev) => ({
+                          ...prev,
+                          status: option.value,
+                        }))
+                      }
+                    />
+                  ))}
+                </SelectV2.Menu>
+              ) : null}
+            </SelectV2.Root>
+          </div>
+          <div className="flex items-center gap-[0.8rem] self-end mb-[0.6rem]">
             <span className="flex body_3_14_r text-gray100">
               평가 완료 숨기기
             </span>
