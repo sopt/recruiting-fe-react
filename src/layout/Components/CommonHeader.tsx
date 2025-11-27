3227a97462b09a7db83d5159d29a38b49f39c23b
namdaeun <nde40345@gmail.com>
2025-11-28 03:12:15 +0900

    feat: 지원서 미리보기 페이지 전용 헤더 구현

--- src/layout/Components/CommonHeader.tsx
+++ src/layout/Components/CommonHeader.tsx
@@ -1,23 +1,32 @@
-import LogoutButton from '@/layout/Components/LogoutButton';
-import { ROUTES_CONFIG } from '@/routes/routeConfig';
 import { useLocation } from 'react-router-dom';
+import LogoutButton from '@/layout/Components/LogoutButton';
+import PreviewFormHeader from '@/layout/Components/PreviewFormHeader';
+import { ROUTES_CONFIG } from '@/routes/routeConfig';
 
 const CommonHeader = () => {
   const { pathname } = useLocation();
 
+  const isPreviewForm = pathname === ROUTES_CONFIG.previewForm.path;
+
   const title = Object.values(ROUTES_CONFIG).find(
-    (route) => route.path === pathname,
+    (route) => route.path === pathname
   )?.title;
 
   return (
-    <header className="realtive w-full h-[13.6rem] bg-background">
-      <div className="fixed w-[100vw] h-[8rem] z-[100] bg-background">
-        <LogoutButton />
-      </div>
-      <h1 className="absolute top-[8.8rem] title_1_32_sb text-gray10 ml-[33.6rem]">
-        {title}
-      </h1>
-    </header>
+    <>
+      {isPreviewForm ? (
+        <PreviewFormHeader />
+      ) : (
+        <header className="realtive w-full h-[13.6rem] bg-background">
+          <div className="fixed w-[100vw] h-[8rem] z-[100] bg-background">
+            <LogoutButton />
+          </div>
+          <h1 className="absolute top-[8.8rem] title_1_32_sb text-gray10 ml-[33.6rem]">
+            {title}
+          </h1>
+        </header>
+      )}
+    </>
   );
 };
 
