5bb37da2c9732594c454e9af77659c488c140ea4
namdaeun <nde40345@gmail.com>
2025-11-28 19:29:56 +0900

    refactor: aria-label 추가

--- src/pages/PostQuestion/components/PreviewButton.tsx
+++ src/pages/PostQuestion/components/PreviewButton.tsx
@@ -15,6 +15,7 @@ const PreviewButton = ({ ...props }: PreviewButtonProps) => {
     <button
       type="button"
       onClick={미리보기페이지로이동}
+      aria-label="뒤로 가기"
       className="label_4_12_sb h-[fit-content] text-gray30 whitespace-nowrap disabled:text-gray500 disabled:cursor-not-allowed cursor-pointer py-[0.4rem] border-b-[0.8px] border-b-transparent hover:border-gray30 disabled:hover:border-b-transparent"
       {...props}
     >
