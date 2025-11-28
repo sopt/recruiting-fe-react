5422dd6a9d3850d0ffc7e0f6be24eb832b0d5165
namdaeun <nde40345@gmail.com>
2025-11-28 19:19:11 +0900

    feat: 지원서 info 섹션 구현

--- src/pages/PreviewForm/PreviewForm.tsx
+++ src/pages/PreviewForm/PreviewForm.tsx
@@ -1,9 +1,11 @@
 import ApplyHeader from '@/pages/PreviewForm/components/ApplyHeader';
+import ApplyInfo from '@/pages/PreviewForm/components/ApplyInfo';
 
 const PreviewForm = () => {
   return (
     <div className="w-full p-8 bg-white min-h-screen">
       <ApplyHeader />
+      <ApplyInfo />
     </div>
   );
 };
