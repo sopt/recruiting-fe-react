95e3db936771cffebebf0fe910ccd966816f483d
namdaeun <nde40345@gmail.com>
2025-11-29 00:33:47 +0900

    feat: 카테고리 탭 구현

--- src/pages/PreviewForm/PreviewForm.tsx
+++ src/pages/PreviewForm/PreviewForm.tsx
@@ -1,11 +1,13 @@
+import ApplyCategory from '@/pages/PreviewForm/components/ApplyCategory';
 import ApplyHeader from '@/pages/PreviewForm/components/ApplyHeader';
 import ApplyInfo from '@/pages/PreviewForm/components/ApplyInfo';
 
 const PreviewForm = () => {
   return (
-    <div className="w-full p-8 bg-white min-h-screen">
+    <div className="w-full p-8 gap-[5rem] flex flex-col bg-white min-h-screen">
       <ApplyHeader />
       <ApplyInfo />
+      <ApplyCategory />
     </div>
   );
 };
