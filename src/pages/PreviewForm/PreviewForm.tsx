ee2c62a099c4470f506bf0150170a8dff97bf7ee
namdaeun <nde40345@gmail.com>
2025-11-29 02:28:56 +0900

    feat: 인적사항 섹션 추가

--- src/pages/PreviewForm/PreviewForm.tsx
+++ src/pages/PreviewForm/PreviewForm.tsx
@@ -1,13 +1,15 @@
 import ApplyCategory from '@/pages/PreviewForm/components/ApplyCategory';
 import ApplyHeader from '@/pages/PreviewForm/components/ApplyHeader';
 import ApplyInfo from '@/pages/PreviewForm/components/ApplyInfo';
+import DefaultSection from '@/pages/PreviewForm/components/DefaultSection';
 
 const PreviewForm = () => {
   return (
-    <div className="w-full p-8 gap-[5rem] flex flex-col bg-white min-h-screen">
+    <div className="w-full p-8 gap-[5rem] flex flex-col bg-white min-h-screen items-center">
       <ApplyHeader />
       <ApplyInfo />
       <ApplyCategory />
+      <DefaultSection />
     </div>
   );
 };
