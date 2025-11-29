caf0253980f14d87d9136d7dd25cfade33746d9e
namdaeun <nde40345@gmail.com>
2025-11-30 02:28:12 +0900

    feat: 공통 질문 리스트 api get

--- src/pages/PreviewForm/PreviewForm.tsx
+++ src/pages/PreviewForm/PreviewForm.tsx
@@ -1,3 +1,4 @@
+import { useGetQuestionList } from '@/pages/PostQuestion/hooks/queries';
 import ApplyCategory from '@/pages/PreviewForm/components/ApplyCategory';
 import ApplyHeader from '@/pages/PreviewForm/components/ApplyHeader';
 import ApplyInfo from '@/pages/PreviewForm/components/ApplyInfo';
@@ -5,6 +6,22 @@ import CommonSection from '@/pages/PreviewForm/components/CommonSection';
 import DefaultSection from '@/pages/PreviewForm/components/DefaultSection';
 
 const PreviewForm = () => {
+  // TODO: season과 group을 전역 상태에서 받아오도록
+  const { data: questionData } = useGetQuestionList(38, 'YB');
+
+  const commonQuestions = questionData?.commonQuestions.map(
+    (question, index) => ({
+      id: question.id || index + 1,
+      question: question.content,
+      charLimit: question.charLimit || 0,
+      placeholder: question.placeholder || '내용을 입력해주세요',
+      urls: question.link ? [question.link] : undefined,
+      isFile: question.isFile,
+      optional: !question.required,
+      isDescription: question.isDescription,
+    })
+  );
+
   return (
     <div className="w-full p-8 flex flex-col bg-white min-h-screen items-center">
       <ApplyHeader />
@@ -12,7 +29,7 @@ const PreviewForm = () => {
         <ApplyInfo />
         <ApplyCategory />
         <DefaultSection />
-        <CommonSection />
+        <CommonSection questions={commonQuestions} />
       </div>
     </div>
   );
--- src/pages/PreviewForm/components/CommonSection.tsx
+++ src/pages/PreviewForm/components/CommonSection.tsx
@@ -1,49 +1,6 @@
 import type { ReactElement } from 'react';
 import Textarea from './Textarea';
 
-const QUESTIONS = [
-  {
-    id: 1,
-    question:
-      '지원자님이 생각하는 공유의 가치가 무엇인지, 지식 또는 경험을 공유하거나 공유받았던 경험을 토대로 서술해 주세요.',
-    charLimit: 700,
-    placeholder: '내용을 입력해주세요',
-    isFile: true,
-    optional: false,
-    isDescription: true,
-  },
-  {
-    id: 2,
-    question:
-      '지원자님의 의지로 시작한 도전 경험을 도전 계기와 과정, 그때 배운 점과 함께 구체적으로 설명해 주세요.',
-    charLimit: 700,
-    placeholder: '내용을 입력해주세요',
-    isFile: false,
-    optional: false,
-    isDescription: false,
-  },
-  {
-    id: 3,
-    question:
-      '팀원 간의 의사소통이 원활하게 되지 않거나, 서로를 신뢰하기 어려웠던 경험이 있으신가요? 그 상황을 어떻게 해결해 나갔는지 구체적으로 작성해 주세요.',
-    charLimit: 700,
-    placeholder: '내용을 입력해주세요',
-    isFile: false,
-    optional: false,
-    isDescription: false,
-  },
-  {
-    id: 4,
-    question:
-      '협업 시 팀원이 지원자님에 대해 표현한 말 중 가장 인상 깊었던 말을 말씀해 주시고, 그때의 상황과 인상 깊었던 이유를 설명해 주세요.',
-    charLimit: 700,
-    placeholder: '내용을 입력해주세요',
-    isFile: false,
-    optional: false,
-    isDescription: false,
-  },
-];
-
 interface Question {
   id: number;
   question: string;
@@ -63,7 +20,7 @@ interface CommonSectionProps {
 }
 
 const CommonSection = ({
-  questions = QUESTIONS,
+  questions,
   infoComponent,
   fileInputComponent,
   linkInputComponent,
@@ -71,7 +28,7 @@ const CommonSection = ({
   return (
     <section id="common" className="flex flex-col gap-[5rem] w-[72rem]">
       <h2 className="w-[72rem] title_2_28_sb text-gray-950">공통 질문</h2>
-      {questions.map(
+      {questions?.map(
         ({
           question,
           id,
