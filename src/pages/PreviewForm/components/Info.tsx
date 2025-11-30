5ff9d8b3c2c2ad064f45797ea861d99e43b51c8a
namdaeun <nde40345@gmail.com>
2025-11-30 18:58:04 +0900

    feat: Info 컴포넌트 추가

---
+++ src/pages/PreviewForm/components/Info.tsx
@@ -0,0 +1,15 @@
+const Info = ({ value }: { value: string }) => {
+  return (
+    <article>
+      <ol className="flex flex-col gap-[1.6rem] mt-[2rem] mb-[5rem]">
+        {value?.split('\n').map((text, idx) => (
+          <li className="body_1_18_m text-gray-300" key={`${idx},${text}`}>
+            {text}
+          </li>
+        ))}
+      </ol>
+    </article>
+  );
+};
+
+export default Info;
