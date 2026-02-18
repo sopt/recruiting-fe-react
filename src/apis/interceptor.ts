c6b86eb3ca50abbf106038446a6f5b90b2bbf4a1
namdaeun <nde40345@gmail.com>
2026-02-18 20:57:38 +0900

    fix: 403 케이스 제외

--- src/apis/interceptor.ts
+++ src/apis/interceptor.ts
@@ -1,12 +1,9 @@
-import { ROUTES_CONFIG } from '@/routes/routeConfig';
 import type { AfterResponseHook, BeforeRetryHook } from 'ky';
 import ky, { HTTPError } from 'ky';
+import { ROUTES_CONFIG } from '@/routes/routeConfig';
 
 export const beforeRetry: BeforeRetryHook = async ({ error }) => {
-  if (
-    error instanceof HTTPError &&
-    (error.response?.status === 401 || error.response?.status === 403)
-  ) {
+  if (error instanceof HTTPError && error.response?.status === 401) {
     window.location.href = ROUTES_CONFIG.login.path;
     return ky.stop;
   }
@@ -17,7 +14,7 @@ export const afterResponse: AfterResponseHook = async (
   _options,
   response,
 ) => {
-  if (response.status === 401 || response.status === 403) {
+  if (response.status === 401) {
     window.location.href = ROUTES_CONFIG.login.path;
   }
   return response;
